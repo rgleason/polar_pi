c8890cd420ecb7465c29413157ecf6f55371140e
Rick Gleason <fcgleason@tgp-architects.com>
2019-03-07 12:21:05 -0500

    change getrow getcol to getnumber--

--- src/PolarDialog.cpp
+++ src/PolarDialog.cpp
@@ -191,7 +191,7 @@ void PolarDialog::OnGridCellChange( wxGridEvent& event )
 			s.Replace(_T("."),_T(","));
 
 /*       m_gridEdit->SetCellValue(s,event.GetRow(),event.GetCol()); */
-         m_gridEdit->SetCellValue(event.GetRows(),event.GetCols(), s);
+         m_gridEdit->SetCellValue(event.GetNumberRows(),event.GetNumberCols(), s);
 	}
 	event.Skip();
 }
