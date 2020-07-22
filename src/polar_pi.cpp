c0ad21dbb4289304f10e8d00e19c23b1ab0e28ea
Rick Gleason <fcgleason@tgp-architects.com>
2020-07-22 00:17:24 -0400

    v1.1.13.0  Frontend2 1.0.127 + updates

--- src/polar_pi.cpp
+++ src/polar_pi.cpp
@@ -71,7 +71,7 @@ extern "C" DECL_EXP void destroy_pi(opencpn_plugin* p)
 //---------------------------------------------------------------------------------------------------------
 
 polar_pi::polar_pi(void *ppimgr)
-      :opencpn_plugin_19(ppimgr)
+      :opencpn_plugin_116(ppimgr)
 {
       // Create the PlugIn icons
       initialize_images();
